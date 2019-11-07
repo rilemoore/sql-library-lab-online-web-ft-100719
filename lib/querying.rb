997f4e752633eb57953b4f59923fb5bea34f2f7f
Riley Moore <rilemoore@gmail.com>
2019-11-07 23:09:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY year";
+  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY(species) ORDER BY COUNT(species) DESC LIMIT 1; "
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY(species) ORDER BY COUNT(species) DESC LIMIT 1 "
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) FROM character_books INNER JOIN characters ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id);"
+  "SELECT characters.name, COUNT(character_books.book_id) FROM character_books INNER JOIN characters ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC"
 end
