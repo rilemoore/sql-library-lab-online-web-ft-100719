2551aaf1343e73a8a8e0c59ae2a5ee52ff577766
Riley Moore <rilemoore@gmail.com>
2019-11-07 22:37:47 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series VALUES(1, "book series", 2, 1);
+INSERT INTO series VALUES(2, "book series", 2, 1);
+
+INSERT INTO books VALUES(1, "Book A", 1995, 1);
+INSERT INTO books VALUES(2, "Book A", 1995, 1);
+INSERT INTO books VALUES(3, "Book A", 1995, 1);
+INSERT INTO books VALUES(4, "Book A", 1995, 1);
+INSERT INTO books VALUES(5, "Book A", 1995, 1);
+INSERT INTO books VALUES(6, "Book A", 1995, 1);
+
+INSERT INTO characters VALUES(1, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(2, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(3, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(4, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(5, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(6, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(7, "Joe", "human","may the force be with you", 2);
+INSERT INTO characters VALUES(8, "Joe", "human","may the force be with you", 2);
+
+INSERT INTO subgenres VALUES (1, "Gothic");
+INSERT INTO subgenres VALUES (2, "Romance");
+
+INSERT INTO authors VALUES (1, "Dr. Seuss");
+INSERT INTO authors VALUES (2, "Dr. Seuss");
+
+INSERT INTO character_books VALUES(1, 2, 3);
+INSERT INTO character_books VALUES(2, 2, 3);
+INSERT INTO character_books VALUES(3, 2, 3);
+INSERT INTO character_books VALUES(4, 2, 3);
+INSERT INTO character_books VALUES(5, 2, 3);
+INSERT INTO character_books VALUES(6, 2, 3);
+INSERT INTO character_books VALUES(7, 2, 3);
+INSERT INTO character_books VALUES(8, 2, 3);
+INSERT INTO character_books VALUES(9, 2, 3);
+INSERT INTO character_books VALUES(10, 2, 3);
+INSERT INTO character_books VALUES(11, 2, 3);
+INSERT INTO character_books VALUES(12, 2, 3);
+INSERT INTO character_books VALUES(13, 2, 3);
+INSERT INTO character_books VALUES(14, 2, 3);
+INSERT INTO character_books VALUES(15, 2, 3);
+INSERT INTO character_books VALUES(16, 2, 3);
